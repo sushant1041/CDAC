36a2c32c4318fcbdef168ebae03bc2a740e8beb3
Sushant Patil <121562905+sushant1041@users.noreply.github.com>
2024-10-07 15:53:19 +0530

    Update Program.cs

--- Dotnet/Assignment/Program.cs
+++ Dotnet/Assignment/Program.cs
@@ -4,13 +4,27 @@ namespace Patient
 {
     class Program
     {
+        public static double Tax(Patient11 inv)
+        {
+            double amount=inv.getBillAmount();
+            double tax = 0;
+            if(amount >= 5000){
+               tax = amount * 0.05;
+            }
+            else if(amount >= 10000)
+            {
+                tax = amount * 0.10;
+            }
+            return tax;
+        }
         static void Main(string[] args)
         {
-            Patient11 p1 = new Patient11(1, "Anna", 10, 1);
+            Patient11 p1 = new Patient11(1, "Anna", 30, 1);
             Console.WriteLine("Total Bill: " + p1.getBillAmount());
 
-            InHouse p2 = new InHouse(1, "Anna", 10, 1); 
+            InHouse p2 = new InHouse(1, "Anna", 30, 1); 
             Console.WriteLine("Total Bill After Discount: " + p2.getBillAmount());
+            Console.WriteLine("Tax is :"+Tax(p1));
         }
     }
 }
