ae85e612aaa67b55c1bd18820741669023478774
Sushant Patil <121562905+sushant1041@users.noreply.github.com>
2024-10-09 22:12:10 +0530

    Update Program.cs

--- Dotnet/Loan/Main/Program.cs
+++ Dotnet/Loan/Main/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 namespace Main;
 using Class;
 
@@ -6,12 +6,20 @@ class Program
 {
     static void Main()
     {
-        PersonalLoan p1 = new PersonalLoan(1500000, 5);
-        HomeLoan hl = new HomeLoan(2500000, 3);
-        Loan p2 = new PersonalLoan(1500000, 5);
-        Loan h2 = new HomeLoan(2500000, 3);
+        PersonalLoan p1 = new PersonalLoan();
+        p1.Principle = 1500000;
+        p1.Period = 5;
+        HomeLoan hl = new HomeLoan();
+        hl.Principle = 2500000;
+        hl.Period = 3;
+        Loan p2 = new PersonalLoan();
+        p2.Principle = 750000;
+        p2.Period = 4;
+        Loan h2 = new HomeLoan();
+        h2.Principle = 1500000;
+        h2.Period = 6;
 
-        Console.WriteLine("EMI for Personal Loan: " + p1.GetEMI());
+        Console.WriteLine("EMI for Personal Loan: "+ p1.GetEMI());
         Console.WriteLine("EMI for Home Loan: " + hl.GetEMI());
         Console.WriteLine("EMI for Personal Loan: " + p2.GetEMI());
         Console.WriteLine("EMI for Home Loan: " + h2.GetEMI());
