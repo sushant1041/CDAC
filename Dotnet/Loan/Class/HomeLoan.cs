694251066d5e7d8d65cbca2fed62e5130e342ade
Sushant Patil <121562905+sushant1041@users.noreply.github.com>
2024-10-11 21:47:37 +0530

    Update HomeLoan.cs

--- Dotnet/Loan/Class/HomeLoan.cs
+++ Dotnet/Loan/Class/HomeLoan.cs
@@ -1,17 +1,23 @@
 namespace Class;
-public class HomeLoan : Loan
+ public interface Discountable{
+    double GetDiscount();
+ } 
+public class HomeLoan : Loan, Discountable
 {
-   // public HomeLoan(double p, float t) : base(p, t) {}
 
     public override float GetRate()
     {
         if (Principle<= 2000000)
         {
-            return 0.10f; // 10%
+            return 0.10f; 
         }
         else
         {
-            return 0.11f; // 11%
+            return 0.11f; 
         }
     }
+    public double GetDiscount()
+    {
+      double result = Principle * GetRate() / 100;
+      return result * 0.05; }
 }
