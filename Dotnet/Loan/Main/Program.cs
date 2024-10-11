611b4d508e34124db5c268f651a4890fe96feecc
Sushant Patil <121562905+sushant1041@users.noreply.github.com>
2024-10-11 21:46:49 +0530

    Update Program.cs

--- Dotnet/Loan/Main/Program.cs
+++ Dotnet/Loan/Main/Program.cs
@@ -1,28 +1,65 @@
 using System;
-namespace Main;
 using Class;
 
-class Program
+namespace Main
 {
-    static void Main()
+    class Program
     {
-        PersonalLoan p1 = new PersonalLoan();
-        p1.Principle = 1500000;
-        p1.Period = 5;
-        HomeLoan hl = new HomeLoan();
-        hl.Principle = 2500000;
-        hl.Period = 3;
-        Loan p2 = new PersonalLoan();
-        p2.Principle = 750000;
-        p2.Period = 4;
-        Loan h2 = new HomeLoan();
-        h2.Principle = 1500000;
-        h2.Period = 6;
+        static void Main()
+        {
+           
+            Loan[] loans = new Loan[4];
+            loans[0] = new HomeLoan { Principle = 2500000, Period = 3 };
+            loans[1] = new PersonalLoan { Principle = 1500000, Period = 5 };
+            loans[2] = new PersonalLoan { Principle = 750000, Period = 4 };
+            loans[3] = new HomeLoan { Principle = 1500000, Period = 6 };
 
-        Console.WriteLine("EMI for Personal Loan: "+ p1.GetEMI());
-        Console.WriteLine("EMI for Home Loan: " + hl.GetEMI());
-        Console.WriteLine("EMI for Personal Loan: " + p2.GetEMI());
-        Console.WriteLine("EMI for Home Loan: " + h2.GetEMI());
+            // display EMI and Tax for each loan
+            foreach (Loan loan in loans)
+            {
+                Console.WriteLine("EMI for " + loan.GetType().Name + " ₹: " + loan.GetEMI().ToString("F2"));
+                if (loan is Taxable taxableLoan)
+                {
+                    Console.WriteLine("Tax for " + loan.GetType().Name + " ₹: " + taxableLoan.GetTax().ToString("F2"));
+                }
+                if (loan is Discountable discountableLoan)
+                {
+                    Console.WriteLine("Discount for " + loan.GetType().Name + " ₹: " + discountableLoan.GetDiscount().ToString("F2"));
+                }
+            }
+
+           
+            double totalDiscount = GetTotalDiscount(loans);
+            double totalTax = GetTotalTax(loans);
+
+            Console.WriteLine("Total Discount on Home Loans ₹: " + totalDiscount.ToString("F2"));
+            Console.WriteLine("Total Tax on Personal Loans ₹: " + totalTax.ToString("F2"));
+        }
+
+        static double GetTotalDiscount(Loan[] loans)
+        {
+            double totalDiscount = 0;
+            foreach (Loan loan in loans)
+            {
+                if (loan is Discountable discountableLoan)
+                {
+                    totalDiscount += discountableLoan.GetDiscount();
+                }
+            }
+            return totalDiscount;
+        }
+
+        static double GetTotalTax(Loan[] loans)
+        {
+            double totalTax = 0;
+            foreach (Loan loan in loans)
+            {
+                if (loan is Taxable taxableLoan)
+                {
+                    totalTax += taxableLoan.GetTax();
+                }
+            }
+            return totalTax;
+        }
     }
 }
-
